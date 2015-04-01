6d9e7557370d56214dffe0e2653e27a33f214e73
anon8 <anon8@chanowa.org>
2015-04-01 14:46:34 +0200

    ...

--- tools/gitref.php
+++ tools/gitref.php
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo '+ <a href='https://github.com/ctrlcctrlv/infinity/'>infinity</a> + <a href='https://github.com/anon8/infinity/'>8ch.pl</a> ' > .installed
+echo -n '+ <a href='https://github.com/ctrlcctrlv/infinity/'>infinity</a> + <a href='https://github.com/anon8/infinity/'>8ch.pl</a> ' > .installed
 git describe --long --tags >> .installed
 
 

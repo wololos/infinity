b6157ab25f2b5b783531813628446ba226eb967f
wololos <chipteken@gmail.com>
2015-04-07 17:52:39 +0200

    Update faq.php
    przetlumaczenie wiekszosci

--- faq.php
+++ faq.php
@@ -10,116 +10,110 @@ $body = <<<EOT
 
 <h2>Jakie są związki 8ch.pl z Chanowa.ORG?</h2>
 <p>Chanowa.ORG zapewnia nam wsparcie techniczne, administracyjne, serwerowe oraz prawne.
-
-<h2>What is an imageboard?</h2>
-<p>An imageboard is a type of internet forum which lets users post text and images anonymously (without name) about any topic, or no topic at all. Unlike forums, imageboards do not hold old content permamently, and old threads are pruned as new ones are created.</p>
  
-<p>The imageboard format holds several advantages against traditional forums:</p>
-<ol>
-<li>There is no registration process, which allows for anyone to post what they like without having to jump through hoops.</li>
-<li>Users do not have names and thus feel no reason to build up an identity or reputation for themselves. Post are judged based on their content rather than who made them.</li>
-<li>sharing images and multimedia content is as easy as saving and uploading it to the site.</li>
-</ol>
+<h2>Czym jest forum obrazkowe (ang. Imageboard)?</h2>
 
-<h2>How is 8chan run?</h2>
-<p>8chan is a service that hosts a large selection of imageboards to browse. These boards are maintained by their respective board owners, who are not affiliated with the 8chan global staff.</p>
-<p>The 8chan global staff are responsible for maintaining the site as a whole and protecting it from spam and illegal content. The administration  is NOT responsible for enforcing any rules outside of the global rule. Any complaints about the content or management of a board should be addressed towards the owner of the board, unless it violates the law of the United States of America, or global policy.</p>
+<p>Imageboard jest typem anonimowego forum internetowego z możliwością, jak sama nazwa wskazuje, postowanie tekstu i obrazu. W przeciwieństwie do typowej grupy dyskusyjnej, nie umożliwia archiwizacji zawartości, i stare wątki są usuwane po utworzeniu nowego (tzw. retencja danych)</p>
+<p>Imageboard ma kilka atutów względem tradycyjnego forum:</p>
+<li>Nie ma procesu rejestracji, co oznacza, że forum jest otwarte dla każdego, by coś napisać bez konieczności przechodzenia formalności.</li>
+<li>Użytkownicy nie posiadają nazw, a tym samym nie ma potrzeby budowania tożsamości lub reputacji. Post są oceniane w oparciu o ich treść, a nie przez pryzmat osoby która je napisała.</li>
+<li>udostępnianie obrazków i treści multimedialnych jest tak łatwe, jak obsługa serwisu</li>
+</ul>
 
-<h2>How do I post a new thread?</h2>
-<p>Navigate to the board you would like to post on, fill out the post form, and click "New Thread". On many boards, you are required to upload an image, but if you do not have one you can also draw a picture by clicking "Oekaki". No boards require you to fill out a name or email address.</p>
+<h2>Jaki jest 8chan?</h2>
+<p>to serwis obsługujący tzw. deski aby przeglądać. Deski te są prowadzonego przez ich właścicieli-założycieli, którzy nie są bezpośrednio powiązani z administracją 8chan. Administrator globalny jest odpowiedzialny za utrzymanie hostingu i ochrony przed spamem i treści niezgodnych z prawem. Administracja nie ponosi odpowiedzialności za egzekwowanie zasad poza globalną regułą. Reklamacje nt. zarządzania boardem należy kierować do właściciela z wybranego działu, chyba że narusza prawa Stanów Zjednoczonych Ameryki, lub globalnej polityki.</p>
+
+<h2>Jak napisać nową wiadomość?</h2>
+<p>Przejdź do wybranego działu, kiedy chcesz napisać post w, wypełnasz pole 'komentarz', i klikasz "nowy temat". Na większości desek, musisz także załadować obrazek, ale jeśli nie masz możesz również narysować, klikając" Oekaki". Nie jest konieczne wypełniania nazwy lub adresu e- mail.</p>
 <img src="/static/faq/new-thread.png" alt="New thread FAQ">
 
-<h2>How do I comment on a thread?</h2>
-<p>On 8chan, threads are ordered by newest to oldest and have no "score" like on other websites. There are no upvotes or "Like" buttons. This allows even unpopular opinions to rise to the top. The replies in threads are ordered oldest to newest and similarly have no score.</p>
-<p>To reply to a thread, click [Reply] on any thread on a board's index.</p>
+<h2>Jak napisać komentarz w wątku?</h2>
+<p>Na 8chan, wątki są sortowane od najnowszych do najstarszych i nie mają" wynik" jak na innych witryn. Brak tu także lajków i innych gadżetów. Pozwala to wyrażanie się na niepopularne tematy i opinie. Odpowiedzi w wątku są sortowane od najstarszych do najnowszych. Aby odpowiedzieć w wątku, kliknij przycisk [Odpowiedz] na wybranym temacie z indeksu.</p>
 <img src="/static/faq/new-reply.png" alt="New thread FAQ">
 
-<p>Fill out your reply. Only the "Comment" field is required for replies on all boards.</p>
+<p>Tylko pole " Komentarz" jest wymagane do odpowiedzi na wszystkich deskach.</p>
 <img src="/static/faq/new-reply2.png" alt="New thread FAQ">
 
-<p>Your reply will be highlighted on the page.</p>
+<p>Twoja odpowiedź będzie podświetlona:</p>
 <img src="/static/faq/new-reply3.png" alt="New thread FAQ">
 
-<h2>How do I reply to another poster?</h2>
-<p>Click the number of their post. A reply box will open automatically prepopulated with the post number you're replying to. Write your post under their post number. Similarly, when people reply to you, it will show (You) after the post number as a hint.</p>
+<h2>Jak odpowiedziec na inny post?</h2>
+<p>Kliknij na liczby znajdujące się pod postem. Pole odpowiedzi otworzy się automatycznie (tzw. 'szybka odpowoedź'). Podobnie, jest kiedy ludzie odpowiadają na twoje, możesz po liczbie rozpoznać swojego posta i traktować jako wskazówkę.</p>
 <img src="/static/faq/new-reply4.png" alt="New thread FAQ">
 
 
-<h2>Are there any global rules regarding content?</h2>
-<p>Only one:</p>
+<h2>Czy są jakieś zasady dotyczące zawartości strony?<h2>
+<p>Tylko jeden:</p>
 <ul>
-<li>Do not post, request, or link to any content illegal in the United States of America. Do not create boards with the sole purpose of posting or spreading such content.</li>
-</ul>
-<p>Other than that, you are free to institute whatever rules you want on your board.</p>
-<p><a href="/obscenity.html">More information about US obscenity laws and how they relate to 8chan boards</a></p>
-<p><a href="/dost.html">More information about the Dost test</a></p>
-<p><a href="/personhood.html">Just who is this 8chan person anyway?</a></p>
+<li>Nie należy zakładać, lub linkować do treści nielegalnych w Stanach Zjednoczonych Ameryki. Nie należy tworzyć boardów z
+Jedynym celem opublikowania lub rozprzestrzeniania takich treści.</li>
+
+<p>(w praktyce banujemy tylko za pornografię dziecięcą)</p>
+
+<p>Poza tym, jesteś wolny, możesz wnieść jakiekolwiek zasady jakie chcesz na swoim boardzie.</p>
 <h2>How do I add more volunteers?</h2>
 <p>You may do this in your board settings, click on "Edit board volunteers".
-<h2>How do I manage my board?</h2>
-<p>Go to <a href="/mod.php">the volunteer panel</a> and click on the board link for your board.</p>
-<h2>How do I contact the admin?</h2>
-<p>The admin can be reached at <tt>admin at 8chan dot co</tt>.</p>
+<h2>Jak zarządzać forum?</h2>
+<p>Zaloguj się i kliknij na link na stronie głównej "zarządzaj swoim boardem"</p>
+<h2>Jak skontaktować się z adminem</h2>
+<p>E-mail: Anon8@chanowa.org</p>
 
 <h2>What's your privacy policy?</h2>
 <p>Find it <a href="/privacy.pdf">here</a>.</p>
 
-<h2>Help! My board has been deleted!</h2>
-<p>As of November 13th, 2014, board expiration no longer occurs.</p>
-
-<p>You still may lose access to your board, however, if you fail to log in for two weeks or it receives no posts for a week. See <a href="/claim.html">here</a> for a list of boards that are available for reclaiming.</p>
+<h2>Pomocy! Moje Forum nie zostało usunięte!</h2>
+<p>Na dzień 13 listopada, 2014, nie występuje wygaszanie desek. Jednak nadal możesz stracić dostęp do deski, jeśli nie zalogujesz się przez dwa tygodnie lub na desce jest brak postów przez tydzień. Zobacz tutaj listę desek, które można przejąć.</p>.
 
 <h2>How do I post as a volunteer on my board?</h2>
 <p>Make sure you are using the volunteer interface to view your board. The URL of your browser should be <a href="/mod.php?/yourboard"><tt>https://8ch.net/mod.php?/yourboard</tt></a>.</p>
 
-<p>If you are the owner of the board, put "## Board Owner" in the name field. If someone else is the owner and you are just assisting them, put "## Board Volunteer" in the name field. Write your post and click "Reply". It will appear with your capcode.</p>
-<h2>Help! The owner of X board is doing something I don't like!</h2>
-<p>If they aren't doing anything illegal, I can't help you. I don't dictate how board owners should manage their boards.</p>
-<p>If they are doing something illegal, email me.</p>
+<h2>Jak zamieścić post jako moderator na swojej desce?</h2>
+<p>Upewnij się, że jesteś zalogowany. W tym celu wpisz adres https:// 8ch. netto/ mod. php?/ yourboard. Jeśli jesteś właścicielem boardu wpisz "## Board owner" w polu Nazwa. Jeśli ktoś inny jest właścicielem a ty jesteś tylko moderatorem, możesz umieścić "## Boards owners" w polu Nazwa. Dodaj swój post i kliknij" Odpowiedz". Pojawi się capcode. (modoznaczek)</p>
+<h2>Pomoc! Właściciel X boardu robi coś czego nie chcesz!</h2>
+<p>Jeśli nie jest to cokolwiek nielegalnego, nie możemy Ci pomóc. Nie dyktujemy jak właściciel boardu powinnem zarządzać deską. Jeśli natomoast robi coś nielegalngo, napisz do administratora seriwsu (kontakt kilka linijek wyżej).</p>
 
-<h2>Can you give me X board?</h2>
-<p>If the owner of the board is inactive or the board is broken due to bad CSS, sure. Send me an email. You can see a list of boards that qualify for being taken over <a href="/claim.html">here</a>.</p>
-
+<h2>Czy możesz dać mi X forum?</h2>
+<p>Jeśli właściciel z wybranego działu jest nieaktywny lub wybrany dział jest uszkodzony ze względu na zły CSS, Wyślij mi e- mail. Możesz wyświetlić <a href="/claim.html">listę desek</a>, które kwalifikują się do przejęcia.</p>
 <h2>Can you add some new feature?</h2>
-<p>Open a <a href="https://github.com/ctrlcctrlv/8chan/issues">Github issue</a>. Better yet, write it yourself and open a pull request.
+<p>Otwórz <a href="https://github.com/anon8/infinity">Githuba</a>. Jeszcze lepiej, napisz to sam i spróbuj wyciągnąć wniosek.</p>.
+<h2>Co jest" sage"?</h2>
+<p>Postujący może odpowiadać w wątkach bez podbijania ich w górę indeksu przez wprowadzenie"sage" w polu e- mail.</p>
 
-<h2>What is "sage"?</h2>
-<p>Posters may reply to threads without bumping them to the top of the index by putting "sage" in the email field.</p>
-
-<h2>What is a tripcode?</h2>
-<p>Most posts on 8chan are made anonymously, but this is not the only way to post. The name field can be used <em>four</em> ways to establish identity:</p>
-<ol>
-<li>By simply writing a name in the box. This is insecure as any other poster can write the same name.</li>
-<li>By writing a # character and then a password. Putting #example in the name field would become !KtW6XcghiY. This is reasonably secure, but with increasing GPU speeds these tripcodes can be cracked in a few days by a dedicated attacker.</li>
-<li>By writing two # characters and then a password. Putting ##example in the name field would become !!Dz.MSNRw9M. This is quite secure, but it relies on a secret salt on the server so the code will not work on sites other than 8chan.</li>
-<li>Board owners and volunteers can enter the special codes "## Board Owner" and "## Board Volunteer" which become <em>capcodes</em> that display after the name. The 8chan administrator can type "## Admin" which becomes <span class="capcode" title="This post was written by the global 8chan administrator."> <i class="fa fa-wheelchair" style="color:blue;"></i> <span style="color:red">8chan Administrator</span></span>.</li>
-</ol>
-<p>Please note, many boards on 8chan have an option set called "Forced anonymity" which causes the name field to not work. This is because many users (and therefore board owners) do not like tripcode users.</p>
+<h2>Co to jest tripcode?</h2>
+<p>Większość postów na 8chan jest wykonywana anonimowo, ale nie jest to jedyny sposób na postowanie. W polu 'Nazwa' można użyć czterech sposobów ujawniania tożsamości.</p>
+<ul>
+<li> Po prostu wpisać swój nick. Jest to niebezpieczne, ponieważ ktoś inny może podpisać skę pod tą samą nazwą.</li>
+<li> Wpisując nick pod postacią login#hasło. Jest to dość rozsądna,opcja, ale wraz ze wzrostem CPU/GPU może zostać złamany w ciągu kilku dni.</li>
+<li> Wpisując dwa krzyżyki czyli login##hasło.  Jest to bardzo bezpieczna metoda, polega na wprowadzeniu tajnego małego pliku na serwerze, więc kod nie będzie działał po za 8chan</li>
+<li> admini i modeatorzy mają możliwość wyróżnienia się po wpisaniu ##board owner lub ## Board Volunteer. ##admin jest zarezerwowane wyłącznie dla właściciela serwera.</li>
+</ul>
+<p>Uwaga, większość boardów na 8chan może nie mieć pola 'Nazwa' po zaznaczeniu opcji o" Wymuszonej anonimowości". To dlatego, że wielu użytkowników(tym samym właściciele) nie lubią używania tripcode na anonimowym forum.</p>
 
 <h2>How do I format my text?</h2>
 <ul>
 <li>**spoiler** or [spoiler]spoiler[/spoiler] -&gt; spoiler</li>
-<li>''italics'' -&gt; <em>italics</em></li>
-<li>'''bold''' -&gt; <strong>bold</strong></li>
-<li>__underline__ -&gt; <u>underline</u></li>
+<li>''italics'' -&gt; <em>kursywa</em></li>
+<li>'''bold''' -&gt; <strong>podgrubienie</strong></li>
+<li>__underline__ -&gt; <u>podkreślenie</u></li>
 <li>==heading== -&gt; <span class='heading'>heading</span> (must be on own line)</li>
-<li>~~strikethrough~~ -&gt; <s>strikethrough</s></li>
-<li>[aa] tags for ASCII/JIS art (escape formatting)</li>
-<li>[code] tags if enabled by board owner</li>
-<li>$$ and \( \) LaTeX tags if enabled by board owner</li>
+<li>~~strikethrough~~ -&gt;<s>przekreślenie</s></li>
+<li>[aa] tags for ASCII/JIS art (wyłącza formatowanie)</li>
+<li>[code] jeśli zostanie włączony przez właściciela deski</li>
+<li>$$ and \( \) tagi LaTeX jeśli zostanie włączony przez właściciela deski</li>
 </ul>
 
 <h2>How are featured boards chosen?</h2>
 <p>Top twenty-five boards excluding /meta/, /b/, /operate/, /boards/ and /news+/.</p>
 
-<h2>Who owns boards like /b/, /news+/ and /operate/?</h2>
-<p>No one, so they are <em>de facto</em> managed by the administration.</p>
+<h2>Kto kieruje deskami /b/ i /meta/?
+<p>Nikt, więc kieruje nimi de facto administracja.</p>
 
 <h2>Why does <a href="/banned">https://8ch.net/banned</a> say that I'm banned? I can still use the boards?</h2>
 <p>8chan is centered around user created boards. That's a board with CSS that makes it look like the ban page, not an official page. You've been tricked. 8chan has no official ban check page.</p>
 
-<h2>Where's the mobile app?</h2>
-<p>There is no official mobile app, however there is an unofficial Android app at <a href="https://github.com/wingy/Exodus/releases">wingy/Exodus</a>.</p>
+<h2>Czy istnieje mobilna aplikacja?</h2>
+<p>Nie ma oficjalnej aplikacji mobilnej, jednak jest nieoficjalna aplikacja Android w wingy/Exldus.<p>
+<p>Nie zapewniamy wsparcia technicznego dla tej aplikacji, zapytaj dewelopera, jeśli masz z nim problem.</p>
 
 <p>I don't provide support for this app, ask the developer of it if you have a problem with it.</p>
 
